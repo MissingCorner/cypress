a3fb9a0502f26e9ac23e222b8149e38d69879c71
Thien Duc <96158924+thienduc7218@users.noreply.github.com>
2023-04-17 11:55:12 +0700

    feat: inital setup (#1)

--- apps/nflow-dev/src/support/app.po.ts
+++ apps/nflow-dev/src/support/app.po.ts
@@ -1,1 +1,5 @@
-export const getGreeting = () => cy.get('h1');
+export const loadCredential = () => {
+  cy.get('#username').type(Cypress.env('username'));
+  cy.get('#password').type(Cypress.env('password'));
+  cy.get('[value="Sign In"]').click();
+};
