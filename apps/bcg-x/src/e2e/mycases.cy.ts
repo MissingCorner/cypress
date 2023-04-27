d6d7780a34dae35383ac3869c729eebf7a452605
sonnguyen <sonnt@missingcorner.com>
2023-04-27 10:01:10 +0700

    feat(bcg): add more tests

--- apps/bcg-x/src/e2e/mycases.cy.ts
+++ apps/bcg-x/src/e2e/mycases.cy.ts
@@ -1,22 +1,22 @@
 import dimensions from '../fixtures/dimensions';
 import { loadCredential } from '../support/app.po';
 
-// describe('Home Page', () => {
-//   beforeEach(() => {
-//     cy.request('/').should((res) => {
-//       expect(res.status).to.eq(200);
-//       expect(res).to.have.property('headers');
-//       expect(res).to.have.property('duration');
-//     });
-//     cy.visit('/');
-//     cy.intercept('https://logon.okta.com/idp/idx/challenge/answer').as('login');
-//     loadCredential();
-//     //check redirect
-//     cy.url().should('contain', 'okta.com');
-//     cy.wait('@login', { timeout: 15000 }).end();
-//   });
+describe('Home Page', () => {
+  beforeEach(() => {
+    cy.request('/').should((res) => {
+      expect(res.status).to.eq(200);
+      expect(res).to.have.property('headers');
+      expect(res).to.have.property('duration');
+    });
+    cy.visit('/');
+    cy.intercept('https://logon.okta.com/idp/idx/challenge/answer').as('login');
+    loadCredential();
+    //check redirect
+    cy.url().should('contain', 'okta.com');
+    cy.wait('@login', { timeout: 15000 }).end();
+  });
 
-// });
+});
 
 describe('Responsive', () => {
   Object.values(dimensions).map((d) => {
